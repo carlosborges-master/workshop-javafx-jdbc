1c6dfd484e7f8e9632a338cd6b1b0b1e9f5a89c9
Carlos Borges <kaca_master@hotmail.com>
2022-10-12 22:42:39 -0300

    About view

---
+++ src/gui/util/Constraints.java
@@ -0,0 +1,35 @@
+package gui.util;
+
+import javafx.scene.control.TextField;
+
+public class Constraints {
+
+	// Limita p digitar so muneros inteiro//
+	public static void setTextFieldInteger(TextField txt) {
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			if (newValue != null && !newValue.matches("\\d*")) {
+				txt.setText(oldValue);
+			}
+		});
+	}
+
+	// Limita mumero maximo de digitos//
+	public static void setTextFieldMaxLength(TextField txt, int max) {
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			if (newValue != null && newValue.length() > max) {
+				txt.setText(oldValue);
+			}
+		});
+
+	}
+
+	// limita p apenas numeros double//
+	public static void setTextFieldDouble(TextField txt) {
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			if (newValue != null && !newValue.matches("\\d*([\\.]\\d*)?")) {
+				txt.setText(oldValue);
+			}
+		});
+
+	}
+}
